9fcae40a3879d9e42ca91b24341727dc840dcb98
Alexandra789 <zakovinko99@gmail.com>
2021-08-11 15:57:25 +0300

    add adaptive

--- src/js/swiperPopularSection.js
+++ src/js/swiperPopularSection.js
@@ -1,19 +1,68 @@
 import Swiper from "swiper";
+let swiperApartment = document.querySelector('.swiper-apartment');
+let swiperHome = document.querySelector('.swiper-home');
+// let screenWidth = window.screen.width;
 
-const swiperPopularSectionFirst = new Swiper('.swiper-apartment', {
-    loop: true,
-    slidesPerView: 2,
-    navigation: {
-        nextEl: '.swiper-apartment-right',
-        prevEl: '.swiper-apartment-left',
-    },
-});
+function choiceSwiper(screenWidth){
+    if (screenWidth <= 540) {
+        console.log(screenWidth);
+        swiperApartment.classList.remove('swiper-apartment');
+        swiperApartment.classList.add('swiper-apartment-adaptive');
+        swiperHome.classList.remove('swiper-home');
+        swiperHome.classList.add('swiper-home-adaptive');
 
-const swiperPopularSectionSecond = new Swiper('.swiper-home', {
-    loop: true,
-    slidesPerView: 2,
-    navigation: {
-        nextEl: '.swiper-home-right',
-        prevEl: '.swiper-home-left',
-    },
-});
+        const swiperPopularSectionFirstAdaptive = new Swiper('.swiper-apartment-adaptive', {
+            loop: true,
+            slidesPerView: 1,
+            navigation: {
+                nextEl: '.swiper-apartment-right',
+                prevEl: '.swiper-apartment-left',
+            },
+        });
+
+
+        const swiperPopularSectionSecondAdaptive = new Swiper('.swiper-home-adaptive', {
+            loop: true,
+            slidesPerView: 1,
+            navigation: {
+                nextEl: '.swiper-home-right',
+                prevEl: '.swiper-home-left',
+            },
+        });
+    }
+    else {
+        swiperApartment.classList.add('swiper-apartment');
+        swiperApartment.classList.remove('swiper-apartment-adaptive');
+        swiperHome.classList.add('swiper-home');
+        swiperHome.classList.remove('swiper-home-adaptive');
+        const swiperPopularSectionFirst = new Swiper('.swiper-apartment', {
+            loop: true,
+            slidesPerView: 2,
+            navigation: {
+                nextEl: '.swiper-apartment-right',
+                prevEl: '.swiper-apartment-left',
+            },
+        });
+
+        const swiperPopularSectionSecond = new Swiper('.swiper-home', {
+            loop: true,
+            slidesPerView: 2,
+            navigation: {
+                nextEl: '.swiper-home-right',
+                prevEl: '.swiper-home-left',
+            },
+        });
+    }
+}
+
+window.onload = ()=> {
+    let screenWidth = window.screen.width;
+    choiceSwiper(screenWidth)
+}
+window.addEventListener(`resize`, event => {
+    let screenWidth = window.screen.width;
+    choiceSwiper(screenWidth);
+}, false);
+
+
+
