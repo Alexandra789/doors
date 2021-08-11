85e37ea86307d2c1404819cd103d9391c1cea6f8
Alexandra789 <zakovinko99@gmail.com>
2021-08-11 11:34:54 +0300

    delete files

--- src/js/choiceBottomBtn.js
+++
@@ -1,9 +0,0 @@
-const bottomBtnsWrapper = document.querySelector('.bottom-btns');
-const bottomBtns = document.querySelectorAll('.bottom-btns button');
-
-bottomBtnsWrapper.addEventListener('click', (e) => {
-    bottomBtns.forEach((item) => {
-        item.classList.remove('active');
-    });
-    e.target.classList.contains('active') ? e.target.classList.remove('active') : e.target.classList.add('active');
-});
