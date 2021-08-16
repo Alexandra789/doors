935eb845495769d823247edc9c9d8aa69d6cd7fa
Alexandra789 <zakovinko99@gmail.com>
2021-08-16 15:58:23 +0300

    fix slider

--- src/js/swiperPopularSection.js
+++ src/js/swiperPopularSection.js
@@ -1,101 +1,47 @@
 import Swiper from "swiper";
-let swiperApartment = document.querySelector('.swiper-apartment');
-let swiperHome = document.querySelector('.swiper-home');
-let swiperHelpfulInformation1 = document.querySelector('.swiper-helpful-information');
 
-function choiceSwiper(screenWidth){
-    if (screenWidth <= 540) {
-        console.log(screenWidth);
-
-        swiperApartment.classList.remove('swiper-apartment');
-        swiperApartment.classList.add('swiper-apartment-adaptive');
-        swiperHome.classList.remove('swiper-home');
-        swiperHome.classList.add('swiper-home-adaptive');
-
-        const swiperPopularSectionFirstAdaptive = new Swiper('.swiper-apartment-adaptive', {
-            loop: true,
-            slidesPerView: 1,
-            navigation: {
-                nextEl: '.swiper-apartment-right',
-                prevEl: '.swiper-apartment-left',
-            },
-        });
-
-
-        const swiperPopularSectionSecondAdaptive = new Swiper('.swiper-home-adaptive', {
-            loop: true,
-            slidesPerView: 1,
-            navigation: {
-                nextEl: '.swiper-home-right',
-                prevEl: '.swiper-home-left',
-            },
-        });
-
-
-    }
-    else {
-        swiperApartment.classList.add('swiper-apartment');
-        swiperApartment.classList.remove('swiper-apartment-adaptive');
-        swiperHome.classList.add('swiper-home');
-        swiperHome.classList.remove('swiper-home-adaptive');
-
-
-        const swiperPopularSectionFirst = new Swiper('.swiper-apartment', {
-            loop: true,
+const swiperPopularSectionFirst = new Swiper('.swiper-apartment', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-apartment-right',
+        prevEl: '.swiper-apartment-left',
+    },
+    breakpoints: {
+        540: {
             slidesPerView: 2,
-            navigation: {
-                nextEl: '.swiper-apartment-right',
-                prevEl: '.swiper-apartment-left',
-            },
-        });
+            spaceBetween: 20,
+        },
+    }
+});
 
-        const swiperPopularSectionSecond = new Swiper('.swiper-home', {
-            loop: true,
+const swiperPopularSectionSecond = new Swiper('.swiper-home', {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-home-right',
+        prevEl: '.swiper-home-left',
+    },
+    breakpoints: {
+        540: {
             slidesPerView: 2,
-            navigation: {
-                nextEl: '.swiper-home-right',
-                prevEl: '.swiper-home-left',
-            },
-        });
+            spaceBetween: 20,
+        }
     }
-    if(screenWidth <= 895){
+});
 
-        swiperHelpfulInformation1.classList.remove('swiper-helpful-information');
-        swiperHelpfulInformation1.classList.add('swiper-helpful-information-adaptive');
-        const swiperHelpfulInformation = new Swiper('.swiper-helpful-information-adaptive', {
-            loop: true,
-            slidesPerView: 1,
-            navigation: {
-                nextEl: '.swiper-helpful-information-right',
-                prevEl: '.swiper-helpful-information-left',
-            },
-        });
-    }
-    else {
-        swiperHelpfulInformation1.classList.remove('swiper-helpful-information-adaptive');
-        swiperHelpfulInformation1.classList.add('swiper-helpful-information');
-
-        const swiperHelpfulInformation = new Swiper('.swiper-helpful-information', {
-            loop: true,
+const swiperHelpfulInformation = new Swiper('.swiper-helpful-information', {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-helpful-information-right',
+        prevEl: '.swiper-helpful-information-left',
+    },
+    breakpoints: {
+        895: {
             slidesPerView: 2,
-            navigation: {
-                nextEl: '.swiper-helpful-information-right',
-                prevEl: '.swiper-helpful-information-left',
-            },
-        });
-
+            spaceBetween: 20,
+        },
     }
-}
-
-window.onload = ()=> {
-    let screenWidth = window.screen.width;
-    choiceSwiper(screenWidth)
-};
-
-window.addEventListener(`resize`, event => {
-    let screenWidth = window.screen.width;
-    choiceSwiper(screenWidth);
-}, false);
-
-
-
+});
